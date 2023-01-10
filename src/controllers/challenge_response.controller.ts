9e5ce7c27493a58778c420e79a05bbd2b2f3ab46
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2023-01-10 16:41:46 +0530

    Revert "Update challenge_response.controller.ts"
    This reverts commit 044b2bee3c357f1afc699a080864333a5326df7f.

--- src/controllers/challenge_response.controller.ts
+++ src/controllers/challenge_response.controller.ts
@@ -575,6 +575,7 @@ export default class ChallengeResponsesController extends BaseController {
             const errs: any = [];
             let attachments: any = [];
             let result: any = {};
+            let file_name_prefix: any;
             let s3 = new S3({
                 apiVersion: '2006-03-01',
                 region: process.env.AWS_REGION,
@@ -584,11 +585,10 @@ export default class ChallengeResponsesController extends BaseController {
             if (!req.files) {
                 return result;
             }
-            let file_name_prefix: any;
-            if (process.env.NODE_ENV == "prod") {
-                file_name_prefix = `ap_ideas/${team_id}`
+            if (process.env.DB_HOST == "ls-ea6dc83d3df52f512dfbfa20261768bab79a69a4.chen5dyzmtrc.ap-south-1.rds.amazonaws.com") {
+                file_name_prefix = `ideas/stage/${team_id}`
             } else {
-                file_name_prefix = `ap_ideas/stage/${team_id}`
+                file_name_prefix = `ideas/${team_id}`
             }
             for (const file_name of Object.keys(files)) {
                 const file = files[file_name];
